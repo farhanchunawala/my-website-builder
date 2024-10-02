d04354a8b029a438455aeb110a1ce3ddd7961619
farhan <farhanchunawala.fc@gmail.com>
2024-10-02 22:52:03 +0530

    VideoDisplayCm template

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import styles from "./page.module.scss";
 import Banner from "../sections/Banners/BannerCm";
+import VideoDisplay from "../sections/VideoDisplays/VideoDisplayCm";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<Banner />
+			<VideoDisplay />
 		</div>
 	);
 }
---
+++ sections/VideoDisplays/VideoDisplayCm.tsx
@@ -0,0 +1,26 @@
+import styles from "./VideoDisplayCm.module.scss";
+
+export default function VideoDisplay() {
+	return (
+		<div className={styles.youtube_home}>
+			<div className={styles.text_container}>
+				<h4>
+					Our <span>Expertise</span>
+				</h4>
+				<p className={styles.para1}>Cancer Care You Can Trust</p>
+				<p className={styles.para2}>
+					At CancerMitr, we understand that every step of your cancer
+					journey is a milestone towards hope and healing. Guided by
+					empathy, and powered by smart technology, we ensure swift
+					access to quality & affordable care
+				</p>
+			</div>
+			<iframe
+				width="695"
+				height="420"
+				src="https://www.youtube.com/embed/x0Rheom9Qh0?autoplay=1"
+				allowFullScreen
+			></iframe>
+		</div>
+	);
+}
