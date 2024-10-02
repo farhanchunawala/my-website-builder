542739a9d434034a8242920f621c685fcfdadf73
farhan <farhanchunawala.fc@gmail.com>
2024-10-03 01:20:39 +0530

    templates folder name change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 import styles from "./page.module.scss";
-// import Banner from "../sections/Banners/BannerCm";
-import VideoDisplay from "../sections/VideoDisplays/VideoDisplayCm";
+import Template001 from "../sections/template001";
 import Template002 from "../sections/template002";
 
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			{/* <Banner /> */}
+			<Template001 />
 			<Template002 />
-			<VideoDisplay />
 		</div>
 	);
 }
