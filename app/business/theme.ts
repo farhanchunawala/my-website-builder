6ce247fc1b57844740d3ac5130dc7cd9f15f3197
Farhan Chunawala <farhanchunawala.fc@gmail.com>
2024-11-02 14:44:06 +0530

    useResponsive and theme.ts

--- app/business/theme.ts
+++ app/business/theme.ts
@@ -2,12 +2,13 @@ import { Typography } from "@/app/configs/global.types";
 import { useResponsive } from "@/app/hooks/useResponsive";
 
 interface Tokens {
-    fonts: {
-        primary: string;
-    };
-    colors: object;
     lineHeight: string;
 }
+
+interface Fonts {
+	primary: string;
+}
+
 
 interface Texts {
 	headingJumbo: Typography;
@@ -20,34 +21,36 @@ const useTheme = () => {
     const { responsive } = useResponsive();
 
     const tokens: Tokens = {
-        fonts: {
-            primary: "var(--font-montserrat)",
-        },
-        colors: {},
         lineHeight: "normal",
     };
-
+	
+	const fonts: Fonts = {
+		primary: "var(--font-montserrat)",
+	};
+	
+	const colors = {};
+	
     const texts: Texts = {
         headingJumbo: {
-            fontFamily: tokens.fonts.primary,
+            fontFamily: fonts.primary,
             fontSize: responsive("36px", "50px", "64px"),
             fontWeight: 400,
         },
 		paragraphBigger: {
-			fontFamily: tokens.fonts.primary,
+			fontFamily: fonts.primary,
 			fontSize: responsive("16px", "20px", "20px"),
 			fontWeight: 400,
             lineHeight: "1.7",
 		},
         hyperlink: {
-            fontFamily: tokens.fonts.primary,
+            fontFamily: fonts.primary,
             fontSize: "12px",
             fontWeight: 500,
             letterSpacing: "1px",
             lineHeight: "1.5",
         },
         buttonText: {
-            fontFamily: tokens.fonts.primary,
+            fontFamily: fonts.primary,
             fontSize: "12px",
             fontWeight: 400,
             letterSpacing: "2px",
@@ -69,7 +72,7 @@ const useTheme = () => {
         },
     };
 
-    return { tokens, texts, buttons };
+    return { tokens, fonts, colors, texts, buttons };
 };
 
 export { useTheme };
