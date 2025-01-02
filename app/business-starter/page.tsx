7bfdd1457a335eeb9b048d0bcddbed6c85084fa4
Farhan Chunawala <farhanchunawala.fc@gmail.com>
2025-01-03 02:45:51 +0530

    s014, ctaBlock, textBlock builder

--- app/business-starter/page.tsx
+++ app/business-starter/page.tsx
@@ -3,13 +3,16 @@ import { RootState } from "@/lib/store";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchData, setData } from "@/lib/features/data/dataSlice";
 import { useEffect, useState } from "react";
-// import "./page.css";
+import "./page.scss";
 import variables from "./variables";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme, useTheme } from "./theme";
+import { theme } from "./theme";
 import Box from "@mui/material/Box";
 import { useMapStyles } from "@/lib/hooks/useMapStyles";
 import useMode from "@/lib/hooks/useMode";
+import TextField from "@mui/material/TextField";
+import Fab from "@mui/material/Fab";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
 // import Section011 from "@/sections/s011/v1";
 import Section013 from "@/sections/s013/v1";
 import Section014 from "@/sections/s014/v1";
@@ -21,14 +24,13 @@ import Section018 from "@/sections/s018/v1";
 export default function Home() {
     useMode();
     const dispatch = useDispatch();
-	const { config, content, styles, styleKit } = useSelector(
-		(state: RootState) => state.data.data
-	);
-    const { device } = useSelector(
-        (state: RootState) => state.responsive
+    const mode = useSelector((state: RootState) => state.mode);
+    const { config, content, styles, styleKit } = useSelector(
+        (state: RootState) => state.data.data
     );
     const [mounted, setMounted] = useState(false);
     const { mapStyles } = useMapStyles();
+    const [sidePanel, setSidePanel] = useState(false);
 
     useEffect(() => {
         dispatch(fetchData());
@@ -44,59 +46,92 @@ export default function Home() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Box
-                className="page"
-                sx={{
-                    ...mapStyles(`page`),
-                    // ...mapStyles(styles?.page, styleKit, device),
-                }}
-            >
-                <Section013
-                    styles={styles.s013}
-                    content={content.s013}
-                    config={config.s013}
-                    styleKit={styleKit}
-                    variables={variables}
-                />
-                <Section014 path="s014" />
-                <Section015
-                    styles={styles.s015}
-                    content={content.s015}
-                    config={config.s015}
-                    styleKit={styleKit}
-                    id={content.s014.ctaBlock.buttonLink}
-                />
-                <Section016
-                    styles={styles.s016}
-                    content={content.s016}
-                    config={config.s016}
-                    variables={variables}
-                    styleKit={styleKit}
-                    id={content.s013.navlinks[0].link}
-                />
-                <Section016
-                    styles={styles.s016a}
-                    content={content.s016a}
-                    config={config.s016a}
-                    variables={variables}
-                    styleKit={styleKit}
-                    id={content.s013.navlinks[1].link}
-                />
-                <Section017
-                    styles={styles.s017}
-                    content={content.s017}
-                    config={config.s017}
-                    styleKit={styleKit}
-                    id={content.s013.navlinks[2].link}
-                />
-                <Section018
-                    styles={styles.s018}
-                    content={content.s018}
-                    config={config.s018}
-                    styleKit={styleKit}
-                    id={content.s013.navlinks[3].link}
-                />
+            <Box className="page">
+                <Box
+                    className="page-content"
+                    sx={{
+                        ...mapStyles(`page`),
+                        // ...mapStyles(styles?.page, styleKit, device),
+                    }}
+                >
+                    <Section013
+                        styles={styles.s013}
+                        content={content.s013}
+                        config={config.s013}
+                        styleKit={styleKit}
+                        variables={variables}
+                    />
+                    <Section014 path="s014" />
+                    <Section015
+                        styles={styles.s015}
+                        content={content.s015}
+                        config={config.s015}
+                        styleKit={styleKit}
+                        id={content.s014.ctaBlock.buttonLink}
+                    />
+                    <Section016
+                        styles={styles.s016}
+                        content={content.s016}
+                        config={config.s016}
+                        variables={variables}
+                        styleKit={styleKit}
+                        id={content.s013.navlinks[0].link}
+                    />
+                    <Section016
+                        styles={styles.s016a}
+                        content={content.s016a}
+                        config={config.s016a}
+                        variables={variables}
+                        styleKit={styleKit}
+                        id={content.s013.navlinks[1].link}
+                    />
+                    <Section017
+                        styles={styles.s017}
+                        content={content.s017}
+                        config={config.s017}
+                        styleKit={styleKit}
+                        id={content.s013.navlinks[2].link}
+                    />
+                    <Section018
+                        styles={styles.s018}
+                        content={content.s018}
+                        config={config.s018}
+                        styleKit={styleKit}
+                        id={content.s013.navlinks[3].link}
+                    />
+                </Box>
             </Box>
+            {mode === "builder" && (
+                <>
+                    {sidePanel && (
+                        <Box className="side-bar">
+                            <TextField
+                                id="outlined-basic"
+                                label="Outlined"
+                                variant="outlined"
+                            />
+                            <button
+                                onClick={toggleSidePanel}
+                                aria-label="Close Side Panel"
+                            >
+                                Close Panel
+                            </button>
+                        </Box>
+                    )}
+                    <Fab
+                        color="primary"
+                        aria-label="Save Data"
+                        sx={{
+                            position: "fixed",
+                            bottom: 32,
+                            right: 32,
+                        }}
+                        // onClick={saveData}
+                    >
+                        <SaveAltIcon />
+                    </Fab>
+                </>
+            )}
         </ThemeProvider>
     );
 }
--- sections/s014/v1/index.tsx
+++ sections/s014/v1/index.tsx
@@ -1,17 +1,17 @@
-import { useSelector } from "react-redux";
-import { RootState } from "@/lib/store";
 import { useEffect, useState } from "react";
-import type Props from "./types";
 import Box from "@mui/material/Box";
 import CtaBlock from "@/components/CtaBlock01/v1";
-import useEvent from "@/lib/hooks/useEvent";
+import useDesignFrame from "@/lib/hooks/useDesignFrame";
 import { useMapStyles } from "@/lib/hooks/useMapStyles";
+
+interface Props {
+    path: string;
+}
 
 const Section014: React.FC<Props> = ({ path }) => {
     const { mapStyles } = useMapStyles();
-    const { createHandlers, getOutline } = useEvent();
+    const { designFrame } = useDesignFrame();
     const [mounted, setMounted] = useState(false);
-    const mode = useSelector((state: RootState) => state.mode);
 
     useEffect(() => {
         setMounted(true);
@@ -26,13 +26,8 @@ const Section014: React.FC<Props> = ({ path }) => {
             component="section"
             className="s014"
             id="s014"
-            {...(mode === "builder" &&
-                createHandlers(`${path}.container`))}
-            // {...createHandlers(`${path}.container`)}
-            sx={{
-                ...mapStyles(`${path}.container`),
-                outline: getOutline(`${path}.container`),
-            }}
+            sx={{ ...mapStyles(`${path}.container`) }}
+            {...designFrame(`${path}.container`)}
         >
             <CtaBlock path={`${path}.ctaBlock`} />
         </Box>
