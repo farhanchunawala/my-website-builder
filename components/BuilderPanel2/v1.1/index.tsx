78c85fa3b3f0070288a613549c1bddba3398e2be
Furqan <furqanchunawala@gmail.com>
2025-08-04 14:50:15 +0530

    minor commit by chairman furqan

--- components/BuilderPanel2/v1.1/index.tsx
+++ components/BuilderPanel2/v1.1/index.tsx
@@ -25,6 +25,34 @@ const buttons = [
     },
 ];
 
+const icons = [
+    {
+        svg: (
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+                <path
+                    d="M 0 4 C 0 1.791 1.791 0 4 0 L 14 0 C 16.209 0 18 1.791 18 4 L 18 14 C 18 16.209 16.209 18 14 18 L 4 18 C 1.791 18 0 16.209 0 14 Z M 8 8 L 5 8 C 4.448 8 4 8.448 4 9 C 4 9.552 4.448 10 5 10 L 8 10 L 8 13 C 8 13.552 8.448 14 9 14 C 9.552 14 10 13.552 10 13 L 10 10 L 13 10 C 13.552 10 14 9.552 14 9 C 14 8.448 13.552 8 13 8 L 10 8 L 10 5 C 10 4.448 9.552 4 9 4 C 8.448 4 8 4.448 8 5 Z"
+                    fill="currentColor"
+                ></path>
+            </svg>
+        ),
+    },
+];
+
+const cards = [
+    {
+        text: "Add",
+    },
+    {
+        text: "Add",
+    },
+    {
+        text: "Add",
+    },
+    {
+        text: "Add",
+    },
+];
+
 const BuilderPanel = ({ panel }) => {
     const [openIndex, setOpenIndex] = useState<number | null>(null);
 
@@ -33,7 +61,23 @@ const BuilderPanel = ({ panel }) => {
     };
 
     return panel == "leftPanel" ? (
-        <Box className="side-bar" sx={{ left: 0, flex: "0 0 48px" }}></Box>
+        <>
+            <Box className="side-bar" sx={{ left: 0, width: "48px" }}>
+                {icons.map((icn, index) => (
+                    <Box className="icons-container" key={index}>
+                        <Box className="icon">{icn.svg}</Box>
+                    </Box>
+                ))}
+            </Box>
+
+            <Box className="side-bar" sx={{ left: "47px", width: "260px" }}>
+                <Box className="cards-container">
+                    {cards.map((crd, index) => (
+                        <Box className="card" key={index}> {crd.text}</Box>
+                    ))}
+                </Box>
+            </Box>
+        </>
     ) : (
         <Box className="side-bar" sx={{ right: 0 }}>
             {buttons.map((btn, index) => (
