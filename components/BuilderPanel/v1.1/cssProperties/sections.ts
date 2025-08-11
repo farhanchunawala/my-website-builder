4b3a0ee3417cddaf1c0fd2a11b2a4f27681b8127
Farhan Chunawala <farhanchunawala.fc@gmail.com>
2025-08-12 02:24:44 +0530

    sections.ts

---
+++ components/BuilderPanel/v1.1/cssProperties/sections.ts
@@ -0,0 +1,16 @@
+
+const sections = {
+	typography: {
+		label: "Typography",
+		properties: [
+			"fontFamily",
+			"fontSize",
+			"fontWeight",
+			"fontStyle",
+			"fontVariant",
+			"fontStretch",
+		]
+	},
+};
+
+export default sections;
